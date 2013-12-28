0b5e454ab623fa2b309b7580fcbb8b345833201f
anders3408 <anders3408@yahoo.dk>
2013-12-28 19:28:50 +0100

    minor derp. If this aint changed ramdisk line don't do a single thing.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_KERNEL_CONFIG := find5_defconfig
 TARGET_KERNEL_SOURCE := kernel/qcom/cfx
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=find5 lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=find5 lpj=67677 user_debug=31 msm_rtb.filter=0x20 ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 
