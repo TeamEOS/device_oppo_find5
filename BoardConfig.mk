44cae3de17ae5551596496451982dde240e0eed5
anders3408 <anders3408@yahoo.dk>
2014-02-03 22:08:48 +0100

    Switch to same kernel in another repo.
    Keep find5 out of issues, while doing more cfx kernel.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_VENDOR := oppo
 # Kernel
 TARGET_ARM_EABI_VERSION := 4.8
 TARGET_KERNEL_CONFIG := find5_defconfig
-TARGET_KERNEL_SOURCE := kernel/qcom/cfx
+TARGET_KERNEL_SOURCE := kernel/oppo/find5
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=find5 lpj=67677 user_debug=31 msm_rtb.filter=0x20 ehci-hcd.park=3
